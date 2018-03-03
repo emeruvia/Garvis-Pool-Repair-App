c0134dbf2c224605c311aa4e5fe5d969f1609163
jeffrey <jeffreyfleurent@outlook.com>
2018-03-02 19:06:28 -0500

    Added icons to navigation bar

--- app/src/main/java/com/example/jeffr/garvispoolrepair/HomePage.java
+++ app/src/main/java/com/example/jeffr/garvispoolrepair/HomePage.java
@@ -108,15 +108,11 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_client) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_work_order) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
+        }  else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
@@ -154,7 +150,7 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
 
             calendarView.setDate(System.currentTimeMillis());
 
-            calendarView.setHorizontalScrollBarEnabled(true);
+
 
 
             return rootView;
